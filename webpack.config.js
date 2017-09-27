98db5eca50c1fc4bdd956693644b87315b87c450
Alan MacLeod <amacleod@rskorbital.com>
2017-09-27 09:19:32 +0700

    hello

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+
+var path = require('path');
+
+module.exports = {
+  entry: './main.js',
+  devtool: "inline-sourcemap",
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname,'build')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['env']
+          }
+        }
+      }
+    ]
+  }
+};
